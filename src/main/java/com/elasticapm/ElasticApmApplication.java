096618305ecfd81b517d0abd94618d7e4542fbeb
Philips <brijesh_deb@yahoo.com>
2022-06-24 08:57:02 +0530

    initial commit

---
+++ src/main/java/com/elasticapm/ElasticApmApplication.java
@@ -0,0 +1,13 @@
+package com.elasticapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticapm/ElasticApmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
